8e5d2a7e642a28163e8c62cf0a56078fec760baf
Paul Thanataweenont <p.t.thanataweenont@gmail.com>
2023-01-31 12:58:58 -0700

    backend initiated

---
+++ src/api/kru-paul-website/routes/kru-paul-website.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * kru-paul-website router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::kru-paul-website.kru-paul-website');
---
+++ src/api/kru-paul-website/services/kru-paul-website.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * kru-paul-website service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::kru-paul-website.kru-paul-website');
