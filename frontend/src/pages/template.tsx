c4609775db9cbfdfa592a38f5dc22dcfc9089d2e
Paul Thanataweenont <p.t.thanataweenont@gmail.com>
2023-02-07 18:57:43 -0700

    Update Template

--- frontend/src/pages/template.tsx
+++ frontend/src/pages/template.tsx
@@ -1,19 +1,11 @@
 import axios from 'axios';
-import { Strapi } from '@strapi/strapi';
 import { ArrayBindingElement } from 'typescript';
 
-// axios.get('http://localhost:1337/api/kru-paul-website?populate=deep').then(response => {
-//   console.log(response.data);
-// });
 
 type Props = {
   content: any;
-  ctx: object;
   subMediaCard: ArrayBindingElement
 }
-
-
-
 
 
 export default function About ({content} : Props) {
@@ -21,15 +13,7 @@ export default function About ({content} : Props) {
 
   return (
     <div>
-      <h1>{content.About.Title}</h1>
-      {content.About.subMediaCard.map( (card:any) => {
-        return(
-          <div>
-            {card.Description}
-          </div>
-
-        )
-      })}
+      Adding Content Here
     </div>
   )
 }
@@ -37,6 +21,7 @@ export default function About ({content} : Props) {
 
 About.getInitialProps = async () => {
   try {
+    // change url to that component's api
     const res = await axios.get('http://localhost:1337/api/kru-paul-website?populate=deep');
     const content = res.data.data?.attributes;
     return {content}
