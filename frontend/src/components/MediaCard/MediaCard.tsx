35a49017a21497920e79ee057b0f01fdd3ce8fe8
Paul Thanataweenont <p.t.thanataweenont@gmail.com>
2023-02-08 11:19:06 -0700

    Update styling

--- frontend/src/components/MediaCard/MediaCard.tsx
+++ frontend/src/components/MediaCard/MediaCard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import RichText from "./util/RichText";
 import styles from "../MediaCard/MediaCard.module.scss";
 import ReactMarkdown from 'react-markdown'
+import { detectContentType } from "next/dist/server/image-optimizer";
 
 
 
@@ -10,14 +10,25 @@ type Props = {
 }
 
 
+
 export default function MediaCard ({content} : Props) {
+  const orientation : React.CSSProperties =
+    {
+      alignItems: `${content.MediaOrientation === 'right' ? "flex-end" : "flex-start"}`
+    }
   return (
     <>
       <div
-        className={styles.wrapper}
+        className={styles.component}
       >
-        <div>Image Area</div>
-        <ReactMarkdown>{content.Description}</ReactMarkdown>
+        <div className={styles.wrapper} style={orientation}>
+          <div
+            className={styles.inner}
+          >
+            <div>Image Area</div>
+            <ReactMarkdown>{content.Description}</ReactMarkdown>
+          </div>
+        </div>
       </div>
     </>
   )
