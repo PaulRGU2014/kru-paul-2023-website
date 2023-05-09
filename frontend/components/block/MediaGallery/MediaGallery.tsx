df42c1a05a9ba658a3c2182ce8a0e0382bf725a8
Paul Thanataweenont <paul@chronos.email>
2023-05-08 21:24:38 -0600

    update typescript

--- frontend/components/block/MediaGallery/MediaGallery.tsx
+++ frontend/components/block/MediaGallery/MediaGallery.tsx
@@ -2,22 +2,24 @@ import React from 'react';
 import styles from './MediaGallery.module.scss';
 import RichText from '../../util/RichText/RichText';
 
-export default function MediaGallery({content}) {
+
+
+export default function MediaGallery({content}:any) {
   return(
     <div className={styles.component}>
       <div className={styles.wrapper}>
         <div className={styles.inner}>
-          {content.cards.map((card, index) => {
+          {content.cards?.map(card => {
             return (
-              <div className={styles.card} key={index}>
-                <RichText html={card.desc}/>
-                
+              <div 
+                className={styles.card} 
+                key={card.id}
+              >
+                <RichText html={card.desc} children={null}/>
               </div>
             )
           })}
-
         </div>
-        <RichText html={content}/>
       </div>
     </div>
   );
