383c3245088af48227a6f14a220f7e3fca53b3d9
Paul Thanataweenont <paul@chronos.email>
2024-07-04 15:05:51 -0600

    update new homepage

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,19 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <p>
+        Thank you for visiting my website. I am a software engineer with a passion for web development.
+      </p>
       <div className={styles.description}>
-        <p>
-          This page is the start of my new website
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
+        <Link href="/about-me">About Me</Link>
+        <Link href="/skills">My Skills</Link>
+        <Link href="/projects">My Projects</Link>
+        <Link href="/contact">Contact Me</Link>
+        <Link href="/online-resume">My Online Résumé</Link>
       </div>
     </main>
   );
