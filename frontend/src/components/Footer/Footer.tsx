23d7c218fcf6004babd9c9115f447e203b8df91e
Paul Thanataweenont <paul@chronos.email>
2024-07-05 12:54:08 -0600

    update anchor styling

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,19 +1,20 @@
  import Link from 'next/link';
+ import styles from './Footer.module.scss';
 
 export default function Footer() {
     return (
         <footer>
-        <p>
-        This page was hard-coded with Next.js, TypeScript and SCSS.
-        </p>
-        <div>
-        <Link href="/">Home</Link>
-        <Link href="/about-me">About Me</Link>
-        <Link href="/skills">My Skills</Link>
-        <Link href="/projects">My Projects</Link>
-        <Link href="/contact">Contact Me</Link>
-        <Link href="/online-resume">My Online Résumé</Link>
-        </div>
+            <div className={styles.link_wrapper}>
+                <Link href="/">Home</Link>
+                <Link href="/about-me">About Me</Link>
+                <Link href="/skills">My Skills</Link>
+                <Link href="/projects">My Projects</Link>
+                <Link href="/contact">Contact Me</Link>
+                <Link href="/online-resume">My Online Résumé</Link>
+            </div>
+            <p>
+                This page was hard-coded with Next.js, TypeScript and SCSS.
+            </p>
         </footer>
     )
 }
