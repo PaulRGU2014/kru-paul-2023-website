2958c751af275ab0f4cbf4ffc3a3f83ebfaf6cd9
Paul Thanataweenont <paul@chronos.email>
2024-08-10 00:56:01 -0600

    update layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
       <body>
         <div className="content-area">
           {children}
-        <Footer />
         </div>
+        <Footer />
       </body>
     </html>
   )
--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,19 +1,21 @@
- import Link from 'next/link';
- import styles from './Footer.module.scss';
+import Link from 'next/link';
+import styles from './Footer.module.scss';
 
 export default function Footer() {
     return (
         <footer className={styles.component}>
-            <p className={styles.disclaimer}>
-                This page was hard-coded with Next.js, TypeScript and SCSS, by Paul Thanataweenont
-            </p>            
-            <div className={styles.link_wrapper}>
-                <Link href="/">Home</Link>
-                <Link href="/about-me">About Me</Link>
-                <Link href="/skills">My Skills</Link>
-                <Link href="/projects">My Projects</Link>
-                <Link href="/contact">Contact Me</Link>
-                <Link href="/online-resume">My Online Résumé</Link>
+            <div className={styles.inner}>
+                <p className={styles.disclaimer}>
+                    This page was hard-coded with Next.js, TypeScript and SCSS, by Paul Thanataweenont
+                </p>
+                <div className={styles.link_wrapper}>
+                    <Link href="/">Home</Link>
+                    <Link href="/about-me">About Me</Link>
+                    <Link href="/skills">My Skills</Link>
+                    <Link href="/projects">My Projects</Link>
+                    <Link href="/contact">Contact Me</Link>
+                    <Link href="/online-resume">My Online Résumé</Link>
+                </div>
             </div>
         </footer>
     )
