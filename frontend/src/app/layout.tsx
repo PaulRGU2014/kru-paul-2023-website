60ad6411ed38d83cee1d7c1da1363ef0d835f046
Paul Thanataweenont <paul@chronos.email>
2024-08-11 23:00:58 -0600

    bug fix

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,27 +3,11 @@
 import Head from "next/head"
 import '../scss/global.scss'
 import Footer from '../components/Footer/Footer'
-import { useEffect, useState } from "react";
-import InViewAnim from '../utils/InViewAnim/InViewAnim'
-import { usePathname } from 'next/navigation';
 
 
 
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
-
-  const pathname = usePathname()
-
-  const [isHomePage, setIsHomePage] = useState(false)
-
-  console.log(pathname)
-
-  useEffect(() => {
-    setTimeout(() => {
-      if (pathname === "/")
-      setIsHomePage (true)
-    }, 1100);
-  }, [pathname])
   
   return (
     <html lang="en">
