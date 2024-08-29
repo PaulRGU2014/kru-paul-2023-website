442cfbfce232bafb4300dc5f083df102b3ca014e
Paul Thanataweenont <paul@chronos.email>
2024-08-29 16:50:59 -0600

    update fields

--- frontend/src/sanity/schemaTypes/galleryCollage.ts
+++ frontend/src/sanity/schemaTypes/galleryCollage.ts
@@ -28,6 +28,21 @@ export const galleryCollage = defineType({
               of: [
                 {
                   type: 'block',
+                  styles: [
+                    {title: 'Normal', value: 'normal'},
+                    {title: 'H1', value: 'h1'},
+                    {title: 'H2', value: 'h2'}
+                  ],
+                  lists: [
+                    {title: 'Numbered', value: 'number'}
+                  ],
+                  marks: {
+                    // Only allow these decorators
+                    decorators: [
+                      {title: 'Strong', value: 'strong'},
+                      {title: 'Emphasis', value: 'em'}
+                    ],    
+                  }
                 },
               ],
             },
