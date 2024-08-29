192721cb7e15f9650be6b7050cbcffd6f36725c1
Paul Thanataweenont <paul@chronos.email>
2024-08-29 01:05:24 -0600

    update pathname

--- frontend/src/app/[slug]/page.tsx
+++ frontend/src/app/[slug]/page.tsx
@@ -3,18 +3,21 @@
 import '../../scss/global.scss'
 import client from "../../../client"
 import { useState, useEffect } from 'react'
+import { usePathname } from 'next/navigation'
 
 
 export default function Page({children}: {children: React.ReactNode}) {
   const [data, setData] = useState(null);
+  const pathname = usePathname();
+  console.log('pathname', pathname);
   useEffect(() => {
     async function fetchData() {
-      const query = `*[_type=="pages"]{...,components[]->}`;
+      const query = `*[_type=="pages"&& page_url.current=="${pathname}"]{...,components[]->}`;
       const res = await client.fetch(query);
       setData(res);
     }
     fetchData();
-  }, []);
+  }, [pathname]);
 
   if (!data) {
     return <div>Loading...</div>;
