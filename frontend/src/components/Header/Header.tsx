a2a14cbf30960edb770251ce1f7a65b295fe4f6e
Paul Thanataweenont <paul@chronos.email>
2024-09-09 14:32:48 -0600

    add backend link

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -6,28 +6,33 @@ import styles from './Header.module.scss';
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logo}>
-        <Link href="/">
-          <Image src="/Logo_big.svg" alt="Logo" width={70} height={70} />
-        </Link>
-        <h4>Paul Thanataweenont</h4>
+      <div className={styles.wrapper}>
+        <div className={styles.logo}>
+          <Link href="/" className={styles.logo_main}>
+            <Image src="/Logo_big.svg" alt="Logo" width={72} height={72} style={{objectPosition: "center", objectFit: "cover"}} />
+          </Link>
+          <h5>Paul Thanataweenont</h5>
+        </div>
+        <nav className={styles.nav}>
+          <ul>
+            {/* <li>
+              <Link href="/">Home</Link>
+            </li> */}
+            <li>
+              <Link href="/about-me">About</Link>
+            </li>
+            <li>
+              <Link href="/showcase">Showcase</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link href="/backend">Login</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <nav className={styles.nav}>
-        <ul>
-          {/* <li>
-            <Link href="/">Home</Link>
-          </li> */}
-          <li>
-            <Link href="/about-me">About</Link>
-          </li>
-          <li>
-            <Link href="/showcase">Showcase</Link>
-          </li>
-          <li>
-            <Link href="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
     </header>
   );
 };
