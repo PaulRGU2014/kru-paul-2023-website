29525cb6d1707ef6527fde7eb345dda4b9265279
Paul Thanataweenont <paul@chronos.email>
2024-09-11 11:04:25 -0600

    update interaction on scroll

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,15 +1,52 @@
-import React from 'react';
+"use client"
+
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './Header.module.scss';
 
 const Header = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlHeader = () => {
+    if (typeof window !== 'undefined') {
+      if (window.scrollY > lastScrollY) {
+        // Scroll down
+        setIsVisible(false);
+      } else {
+        // Scroll up
+        setIsVisible(true);
+      }
+      setLastScrollY(window.scrollY);
+    }
+  };
+
+  const handleMouseMove = (event: MouseEvent) => {
+    if (event.clientY < 50) {
+      setIsVisible(true);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', controlHeader);
+      window.addEventListener('mousemove', handleMouseMove);
+
+      return () => {
+        window.removeEventListener('scroll', controlHeader);
+        window.removeEventListener('mousemove', handleMouseMove);
+      };
+    }
+  }, [lastScrollY]);
+
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${isVisible ? styles.visible : styles.hidden}`}>
       <div className={styles.wrapper}>
         <div className={styles.logo}>
           <Link href="/" className={styles.logo_main}>
-            <Image src="/Logo_New.svg" alt="Logo" width={150} height={90} style={{objectPosition: "center", objectFit: "cover"}} /><div className={styles.logo_shadow}></div>
+            <Image src="/Logo_New.svg" alt="Logo" width={150} height={90} style={{objectPosition: "center", objectFit: "cover"}} />
+            <div className={styles.logo_shadow}></div>
           </Link>
           <h5>Krupaul.com</h5>
         </div>
